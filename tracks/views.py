3a883e8a1b125e3c406dfe8c05ae3dffd54dac48
Miguel Pérez <godlynxo@outlook.com>
2017-04-30 17:58:31 -0500

    Serializar datos con módulo json

--- tracks/views.py
+++ tracks/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import get_object_or_404, render
-
+import json
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import Track
+
 
 def tracks(request, title):
     track = get_object_or_404(Track, title=title)
-    return render(request, 'tracks.html', { 'track': track})
+    data = {
+        'title': track.title,
+        'order': track.order,
+        'track_file': track.track_file.url,
+        'album': track.album.title,
+        'artist': {
+            'first_name': track.artist.first_name,
+            'last_name': track.artist.last_name,
+            'biography': track.artist.biography
+        }
+    }
+    json_data = json.dumps(data)
+    return HttpResponse(json_data, content_type='application/json')
